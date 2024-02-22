35d8b849e3307874f3f31869fedc63ecea824cc0
Danny Regan <d.regan@columbia.edu>
2024-02-21 21:32:45 -0800

    hero section tab styling, /front end/ styling, photo update

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import styles from './Nav.module.css';
-import hero from '../images/Hero.jpg';
+import hero from '../images/Hero 2.JPG';
 
 export default function Nav() {
     useEffect(() => {
@@ -42,38 +42,39 @@ export default function Nav() {
                     </div>
                     <div className={styles.navDivCenter}>
                         <ul className={styles.navTabs}>
-                            <a href='#About Me'>
-                                <li>about me</li>
+                            <a href='#Portfolio'>
+                                <li>Portfolio</li>
                                 <div className={styles.dots}></div>
                             </a>
-                            <a href='#Portfolio'>
-                                <li>portfolio</li>
+                            <a href='#About Me'>
+                                <li>About</li>
                                 <div className={styles.dots}></div>
                             </a>
                             <a href="#Resume">
-                                <li>resume</li>
+                                <li>Resume</li>
                                 <div className={styles.dots}></div>
                             </a>
                         </ul>
                     </div>
-                    <div className={styles.navDivRight}>
+                    {/* <div className={styles.navDivRight}>
                         <a href='#'>
                             <p className={styles.initials}>blog</p>
                         </a>
-                    </div>
+                    </div> */}
                 </div>
                 <div className={styles.dannyRegan}>
-                    <h1 className={styles.danny}>Danny</h1>
-                    <h1 className={styles.regan}>Regan</h1>
-                    <p className={styles.frontEnd}>{a} <span>{noun}</span></p>
+                    {/* <h1 className={styles.danny}>Danny</h1>
+                    <h1 className={styles.regan}>Regan</h1> */}
+                    <p className={styles.frontEnd}>{a} 
+                     <span> {noun}</span></p>
                 </div>     
-                <div className={styles.valueContainer}>
+                {/* <div className={styles.valueContainer}>
                         <p className={styles.valueStatement}>Here's a text box about my stupid little value statement that says what I bring to the table in a single sentence.</p>
                         <div className={styles.buttonsContainer}>
                             <a href='#' className={[styles.button, styles.contactMe]}><button>Contact Me</button></a>
                             <a href='#' className={[styles.button, styles.downloadMyResume]}><button>Download My Resume</button></a>
                         </div>
-                    </div>     
+                    </div>      */}
                 <div className={styles.motto}>
                     <p>some text, a motto for the bottom right of banner image.</p>
                 </div>
