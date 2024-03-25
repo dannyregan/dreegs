99bc188d4bd1b24a89d13fe665cb2127559ea4d6
Danny Regan <d.regan@columbia.edu>
2024-03-25 15:55:27 -0700

    added the about me information section and styled it

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -2,6 +2,9 @@ import React from "react";
 import about from "./About.module.css";
 import styles from "./Nav.module.css";
 import introduction from "../images/wideIntro.JPG"
+import me from "../images/meAndZ.JPG"
+import myWork from "../images/lookingAtRocks.JPG"
+import myCode from '../images/Hero 3.JPG'
 
 
 export default function AboutMe() {
@@ -17,18 +20,34 @@ export default function AboutMe() {
                 <div className={about.line}></div>
                 <div className={about.aboutInfoContainer}>
                     <div className={about.me}>
-                        <div className={about.title}>Me</div>
-                            <div className={about.information}>
-                                <p className={about.mainPs}>I’m Danny, a software engineer originally from Andover, Massachusetts, now based in Las Vegas, Nevada. I work for an environmental non-profit dedicated to connecting people with the outdoors. My job combines my expertise in biology with my love of the outdoors, which is something I find truly rewarding.</p>
-                            </div>
-                        <div className={about.title}>My Work</div>
-                            <div className={about.information}></div>
-                                <p className={about.mainPs}>This work especially brings me joy when working with English Language Learners. Oftentimes, these students are Latino immigrants who have inspired me to learn Spanish. When my students watch me struggle to get my point across in Spanish, they usually laugh, and almost always feel more comfortable around me, knowing that the struggle to learn a new language is mutual.</p>
-                        <div className={about.title}>My Code</div>
-                        <div className={about.information}></div>
+                        <div className={about.infoSection}>
+                            <h2 className={about.title}>Me</h2>
+                                <div className={about.information}>
+                                    <img src={me} alt='Danny Regan and two dogs.' className={about.threePics}/>
+                                    <div className={about.overlay}></div>
+                                    <p className={about.mainPs}>I’m Danny, a software engineer originally from Andover, Massachusetts. Now, I'm based in Las Vegas, Nevada and I work for an environmental non-profit dedicated to connecting people with the outdoors. My job combines my expertise in biology with my love of the outdoors, which is something I find truly rewarding.</p>
+                                </div>
+                        </div>
+                        <div className={about.infoSection}>
+                            <h2 className={about.title}>My Work</h2>
+                                <div className={about.information}>
+                                    <img src={myWork} alt='Danny Regan and two dogs.' className={about.threePics}/>
+                                    <div className={about.overlay}></div>
+                                        <p className={about.mainPs}>I often work with English Language Learners, many of whom are Latino immigrants, who have inspired me to learn Spanish. When my students watch me struggle with my Spanish, they usually laugh, and almost always feel more comfortable around me, knowing that the struggle to learn a new language is mutual.</p>
+                                </div>
+                        </div>
+                        <div className={about.infoSection}>
+                            <h2 className={about.title}>My Code</h2>
+                                <div className={about.information}>
+                                <img src={myCode} alt='Danny Regan and two dogs.' className={about.threePics}/>
+                                    <div className={about.overlay}></div>
+                                        <p className={about.mainPs}>I aspire to study computer science to gain a holistic and advanced understanding of CS principles and theory. In doing so, I’ll bolster my career as a software engineer, empowering me to drive advancements in sustainable development and environmental conservation, and to improve outdoor accessibility for underrepresented groups.</p>
+                                </div>
+
+                        </div>
+                        <div></div>
                     </div>
                 </div>
-                <p className={about.mainPs}>Since beginning my software engineering journey, I’ve felt inclined to enroll in graduate school to study computer science formally and gain a holistic and advanced understanding of CS principles and theory. In doing so, I’m certain I’ll bolster my career as a software engineer, empowering me to drive societal advancement through technological solutions in renewable energy, sustainable development, and environmental conservation, and to continue to improve outdoor accessibility for underrepresented groups.</p>
             </div>
         </div>
     )
